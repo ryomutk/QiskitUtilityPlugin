2211b69cb7898b3446f442e27fe56dbcf7bab8bb
ryom <ryotagranpa@gmail.com>
2022-01-09 03:37:15 +0900

    Fixed some debug lines

--- Assets/QiskitUtilityAsset/Scripts/QASMComunicator.cs
+++ Assets/QiskitUtilityAsset/Scripts/QASMComunicator.cs
@@ -16,51 +16,6 @@ public class QASMComunicator : Singleton<QASMComunicator>
         return task;
     }
 
-    void Test()
-    {
-
-        /*
-        @"include ""qelib1.inc""; 
-        qreg q[10]; 
-        creg c[10]; 
-        h q[0];
-        h q[1]; 
-        h q[2]; 
-        h q[3]; 
-        h q[4]; 
-        h q[5];
-        h q[6]; 
-        h q[7]; 
-        h q[8]; 
-        h q[9]; 
-        measure q[0] -> c[0]; 
-        measure q[1] -> c[1]; 
-        measure q[2] -> c[2]; 
-        measure q[3] -> c[3]; 
-        measure q[4] -> c[4];
-        measure q[5] -> c[5]; 
-        measure q[6] -> c[6]; 
-        measure q[7] -> c[7]; 
-        measure q[8] -> c[8]; 
-        measure q[9] -> c[9];";
-        */
-        var circuit =
-@"
-    qr = QuantumRegister(10)
-    cr = ClassicalRegister(10)
-    qc = QuantumCircuit(qr,cr)
-
-    for i in range(10):
-        qc.h(i)
-
-    qc.measure(qr,cr)
-
-    return qc";
-
-        var task = new SmallTask<QuantumResult>();
-        StartCoroutine(RunCircuit(circuit, task));
-    }
-
     public SmallTask<QuantumResult> RunCircuitAsync(string method)
     {
         var task = new SmallTask<QuantumResult>();
